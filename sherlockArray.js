237c42f02d17141f42e52463d356fb875fc2c809
Nosisky <nosisky@gmail.com>
2018-02-26 12:01:43 +0100

    Add the sherlock array solution

--- sherlockArray.js
+++ sherlockArray.js
@@ -1,6 +1,6 @@
 const checkSherlock = (arr) => {
-  function isSherlock(elem, i, arr) {
-    function sum(i, n) {
+  const isSherlock = (elem, i, arr) => {
+    const sum = (i, n) => {
       return i < n ? arr[i] + sum(i + 1, n) : 0;
     }
     return sum(0, i) === sum(i + 1, arr.length);
